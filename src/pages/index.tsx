491fc7115b427c59d0a37fa117dc74b98cb6a9d9
giovanniandr <fs_giovanni@hotmail.com>
2022-09-02 16:42:58 +0000

    1st commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,16 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Lexir Frontend Assessment!</title>
-        <meta name="description" content="Lexir Frontend Assessment!" />
+        <title>Billing profile details</title>
+        <meta name="description" content="Billing profile details" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Lexir Frontend Assessment!</a>
+          <a href="https://nextjs.org">Logo here</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
       </main>
     </div>
   );
